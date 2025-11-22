c1a945b3c5f4cf591b9fcb263de1a0adf3a0f45f
Dmitry Popov <dmitriypopovsamara@gmail.com>
2025-11-23 00:29:52 +0100

    version update

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WandererKills.MixProject do
   def project do
     [
       app: :wanderer_kills,
-      version: "1.5.6",
+      version: "1.5.7",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       compilers: Mix.compilers() ++ [:boundary],
