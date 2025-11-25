a66eb06d9f1629426216f83c1536c123a96b05c8
Dmitry Popov <dmitriypopovsamara@gmail.com>
2025-11-25 17:30:09 +0100

    ci updates

--- mix.exs
+++ mix.exs
@@ -1,12 +1,10 @@
 defmodule WandererKills.MixProject do
   use Mix.Project
 
-  @version "1.5.8"
-
   def project do
     [
       app: :wanderer_kills,
-      version: @version,
+      version: "1.5.8",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       compilers: Mix.compilers() ++ [:boundary],
